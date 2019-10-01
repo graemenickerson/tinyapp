7febe05cbefda51eb679bf5e481e74e56b0fb269
graemenickerson <graeme.nickerson@gmail.com>
2019-10-01 20:45:34 +0000

    Adds login/logout functionality with cookies.

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,16 +27,40 @@ app.listen(PORT, () => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+// User can log in and is assigned a cookie
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
 });
 
+// Logs user out and deletes cookie from browser
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+// shows page for adding link to database
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
+});
+
+// Shows page for editing a shortend link
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -45,14 +72,13 @@ const generateRandomString = function() {
     key += data[Math.floor(Math.random() * data.length)];
   }
   return key;
-}
+};
 
 // Adds new url to database for shortening
 app.post("/urls", (req, res) => {
-  const newShortURL = generateRandomString()
+  const newShortURL = generateRandomString();
   urlDatabase[newShortURL] = req.body.longURL;
-  console.log(urlDatabase);  // Log the POST request body to the console
-  res.redirect(`/urls/${newShortURL}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${newShortURL}`);
 });
 
 // Redirects shortUrl to longURL website
@@ -65,7 +91,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
-})
+});
 
 // Updates existing shortend url
 app.post('/urls/:shortURL/update', (req, res) => {
